94ce8a77d5d62d975d74a7b646cf7567cf039e97
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-16 14:47:49 +0530

    Models added

---
+++ BethanyPieShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BethanyPieShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
