e0b27df25fb4c3a3c2d7826d914e0d1a9e035346
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-17 10:52:53 +0530

    Details view added

--- BethanyPieShop/Controllers/PieController.cs
+++ BethanyPieShop/Controllers/PieController.cs
@@ -26,5 +26,15 @@ namespace BethanyPieShop.Controllers
             piesListViewModel.CurrentCategory = "Cheese cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie == null )
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
     }
 }
